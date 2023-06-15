1682b89c95d8c1e21fba19925f9a984582470bbb
kblisstech <kissiwekanayo@gmail.com>
2023-06-15 10:47:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput(STDOUT_FILENO, "and that piece of art is useful\"
-			 - Dora Korpar, 2015-10-19\n", stdout);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}
+}}
